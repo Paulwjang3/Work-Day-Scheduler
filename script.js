ad715531e3659539bf710b02371105197f7fd255
Paulwjang3 <128998742+Paulwjang3@users.noreply.github.com>
2023-04-19 05:14:12 -0400

    final adjustments

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(function () {
     var today = dayjs();
     $('#currentDay').text(today.format('dddd, MMMM D, YYYY h:mm A'));
 
-
+    // Setup to save input text into localstorage
     $('.saveBtn').on('click', function() {
         var inputText = $(this).siblings(".description").val();
         var selectedTime = $(this).parent().attr('id');
@@ -11,8 +11,9 @@ $(function () {
         localStorage.setItem(selectedTime, inputText);
     });
 
-    var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
-
+// An array of normal business hours
+    var hours = ["#hour-9", "#hour-10", "#hour-11", "#hour-12", "#hour-1", "#hour-2", "#hour-3", "#hour-4", "#hour-5"];
+    // This pulls any saved text from the local storage
     $('time-block').each(function() {
         var blockId = $(this).attr("id");
         for (var i = 0; i < hours.length; i++); {
@@ -21,13 +22,14 @@ $(function () {
             }
         }
     });
-
+    // Current time
     function currentTime() {
-       var time = today.format('h');
-
+       var time = dayjs().format('h:mm A');
+        // Compares the listed times with the current time
        $('.time-block').each(function() {
-        var rowTime = parseInt($(this).attr('id'));
-        console.log(rowTime, time)
+        var hourElement = $(this).find('.hour');
+        var rowTime = hourElement.text().trim();
+        console.log(rowTime,time)
 
         if (rowTime < time) {
             $(this).addClass("past");
@@ -51,3 +53,13 @@ $(function () {
     currentTime();
 });
 
+$('time-block').each(function() {
+    var blockId = $(this).attr("id");
+    for (var i = 0; i < hours.length; i++); {
+        if (blockId.includes(hours[i])) {
+            $(this).find('.description').val(localStorage.getItem(blockId));
+        }
+
+    }
+    console.log(blockId);
+});
